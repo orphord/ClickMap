befd8163135e9a089291dae7684b776814f549f7
Jeffrey Orford <orphord@gmail.com>
2014-03-14 15:43:29 -0600

    Minor changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,12 @@
 //#######################
 // A container for middleware functions
 //#######################
-var express = require('express')
-, passport = require('passport')
+var express = require('express'),
+passport = require('passport');
 
 module.exports = {
 	cookieParser: express.cookieParser(),
 	session: express.session({secret: 'yo mama'}),
 	initPassport: passport.initialize(),
 	passportSession: passport.session()
-}
+};
