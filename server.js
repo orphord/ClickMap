ba3be3451d13aeb173a884b5a8876b415b844600
Jeff Orford <orphord@gmail.com>
2018-10-18 15:16:50 -0600

    Changes to update express (to v4) and Google Maps API (to use latest and api key.

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
 // This is a comment to test push
 var express = require('express')
 , http = require('http')
-, path = require('path')
-, passport = require('passport')
-, ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
-, middlewares = require('./config/middlewares');
+, path = require('path');
 
-
+// Make express app object
 var app = express();
 
-// Initialize middleware
-app.use(middlewares.cookieParser);
-app.use(middlewares.session);
-app.use(middlewares.initPassport);
-app.use(middlewares.passportSession);
-app.use(app.router);
-
-// Define routes including a secured route to everything
-app.all('/',
-	ensureLoggedIn('/login'),
-				function(req, res, next) {
-					console.log('Successful Login callback');
-					next();
-				});
+// Set static directory
 app.use('/', express.static(path.join(__dirname, '/public')));
+
+// set routes
 require('./config/routes')(app);
-require('./config/protect')(app);
+
 
 // spin up server
-var listenPort = (process.env.VMC_APP_PORT || 1337);
-app.listen(listenPort);
+console.log('about to set port');
+app.set('port', process.env.VMC_APP_PORT || 3000);
+console.log('about to start server');
 
-console.log('Node server listening at port: ' + listenPort);
+app.listen(app.get('port'), function() {
+	console.log('Node server listening at port: ' + app.get('port'));
+	}
+);
+console.log('server start')
+
+
